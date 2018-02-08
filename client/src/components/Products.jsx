326ac743f8bdbffdcb237eb32ee44873ecb9693f
Jamie <jamie.lo112@gmail.com>
2018-02-08 14:16:31 -0800

    [add]
    Set up gitignore, package.json, webpack, server and basic front-end.

---
+++ client/src/components/Products.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    );
+  }
+}
+
+
+export default App;
